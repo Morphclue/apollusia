51e1c99aa15972fe6cdf8b94e9a975b7e384de55
Julian Holfeld <julian@uni-kassel.de>
2022-04-11 14:05:44 +0200

    feat(fe): generate dashboard c+m

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(p => p.DashboardModule)},
+  {path: '', pathMatch: 'full', redirectTo: 'dashboard'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
