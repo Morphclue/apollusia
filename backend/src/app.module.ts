826d48b562cc46d87d9fcb34986e3223b7011911
Julian Holfeld <julian@uni-kassel.de>
2022-05-11 09:31:59 +0200

    feat: add mongoose and docker

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import {Module} from '@nestjs/common';
+import {MongooseModule} from "@nestjs/mongoose";
+
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 
 @Module({
-    imports: [],
+    imports: [MongooseModule.forRoot(process.env.DB_URL)],
     controllers: [AppController],
     providers: [AppService],
 })
