a2c42110a61b0926e73226e6086a29e68e40eacc
Julian Holfeld <julian@uni-kassel.de>
2022-05-12 15:53:41 +0200

    feat: add apiProperty to pollDto

--- backend/src/dto/poll.dto.ts
+++ backend/src/dto/poll.dto.ts
@@ -1,5 +1,12 @@
+import {ApiProperty} from '@nestjs/swagger';
+
 export class PollDto {
+    @ApiProperty()
     title: string;
+
+    @ApiProperty()
     description?: string;
+
+    @ApiProperty()
     deadline?: any;
 }
