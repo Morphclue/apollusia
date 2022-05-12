1e4b7315914bf76136d6d0531c62492a05c751ea
Julian Holfeld <julian@uni-kassel.de>
2022-05-12 15:36:28 +0200

    feat: setup swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,21 @@
 import {NestFactory} from '@nestjs/core';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
+
 import {AppModule} from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
+
+    const config = new DocumentBuilder()
+        .setTitle('Apollusia')
+        .setDescription('Apollusia API description')
+        .setVersion('0.0.0')
+        .addTag('apollusia')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
     await app.listen(3000);
 }
 
