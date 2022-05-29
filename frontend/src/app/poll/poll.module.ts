a6483502566722a9b6eb387fb0152ab99857dfbb
Julian Holfeld <julian@uni-kassel.de>
2022-05-29 12:00:16 +0200

    feat: add angular-calender with dependencies

--- frontend/src/app/poll/poll.module.ts
+++ frontend/src/app/poll/poll.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
-import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {HttpClientModule} from '@angular/common/http';
+import {CalendarModule, DateAdapter} from 'angular-calendar';
+import {adapterFactory} from 'angular-calendar/date-adapters/date-fns';
 
 import {CreatePollComponent} from './create-poll/create-poll.component';
-import {PollRoutingModule} from "./poll-routing.module";
+import {PollRoutingModule} from './poll-routing.module';
 import {EditPollComponent} from './edit-poll/edit-poll.component';
 import {ChooseDateComponent} from './choose-date/choose-date.component';
 
@@ -23,6 +25,10 @@ import {ChooseDateComponent} from './choose-date/choose-date.component';
     HttpClientModule,
     FormsModule,
     NgbModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
   ],
 })
 export class PollModule {
