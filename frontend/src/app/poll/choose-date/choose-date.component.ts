0b8bec219302d9cdc178b15595670bbfdb76d7f5
Julian Holfeld <julian@uni-kassel.de>
2022-05-29 15:06:39 +0200

    feat: add calendar view with events

--- frontend/src/app/poll/choose-date/choose-date.component.ts
+++ frontend/src/app/poll/choose-date/choose-date.component.ts
@@ -1,16 +1,75 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component} from '@angular/core';
+import {WeekViewHourSegment} from 'calendar-utils';
+import {fromEvent} from 'rxjs';
+import {finalize, takeUntil} from 'rxjs/operators';
+import {addDays, addMinutes, endOfWeek} from 'date-fns';
+
+import {ChooseDateService} from '../services/choose-date.service';
 
 @Component({
   selector: 'app-choose-date',
   templateUrl: './choose-date.component.html',
   styleUrls: ['./choose-date.component.scss'],
 })
-export class ChooseDateComponent implements OnInit {
+export class ChooseDateComponent {
 
-  constructor() {
+  viewDate = new Date();
+  dragToCreateActive = true;
+  weekStartsOn: 1 = 1;
+
+  constructor(
+    private changeDetectorRef: ChangeDetectorRef,
+    private chooseDateService: ChooseDateService,
+  ) {
   }
 
-  ngOnInit(): void {
+  startDragToCreate(
+    segment: WeekViewHourSegment,
+    mouseDownEvent: MouseEvent,
+    segmentElement: HTMLElement,
+  ) {
+    const dragToSelectEvent = this.chooseDateService.createDragSelectEvent(segment);
+    const segmentPosition = segmentElement.getBoundingClientRect();
+    this.dragToCreateActive = true;
+    const endOfView = endOfWeek(this.viewDate, {
+      weekStartsOn: this.weekStartsOn,
+    });
+
+    fromEvent(document, 'mousemove')
+      .pipe(
+        finalize(() => {
+          delete dragToSelectEvent.meta.tmpEvent;
+          this.dragToCreateActive = false;
+          this.refresh();
+        }),
+        takeUntil(fromEvent(document, 'mouseup')),
+      )
+      .subscribe((mouseMoveEvent: any) => {
+        let minutesDifference = this.chooseDateService.ceilToNearest(
+          mouseMoveEvent.clientY - segmentPosition.top,
+          30,
+        );
+
+        const daysDifference =
+          this.chooseDateService.floorToNearest(
+            mouseMoveEvent.clientX - segmentPosition.left,
+            segmentPosition.width,
+          ) / segmentPosition.width;
+
+        const newEnd = addDays(addMinutes(segment.date, minutesDifference), daysDifference);
+        if (newEnd > segment.date && newEnd < endOfView) {
+          dragToSelectEvent.end = newEnd;
+        }
+        this.refresh();
+      });
   }
 
+  refresh() {
+    this.chooseDateService.events = [...this.chooseDateService.events];
+    this.changeDetectorRef.detectChanges();
+  }
+
+  getEvents() {
+    return this.chooseDateService.events;
+  }
 }
--- frontend/src/app/poll/services/choose-date.service.ts
+++ frontend/src/app/poll/services/choose-date.service.ts
@@ -1,9 +1,13 @@
 import {Injectable} from '@angular/core';
+import {CalendarEvent} from 'angular-calendar';
+import {WeekViewHourSegment} from 'calendar-utils';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChooseDateService {
+
+  events: CalendarEvent[] = [];
 
   constructor() {
   }
@@ -14,5 +18,18 @@ export class ChooseDateService {
 
   ceilToNearest(amount: number, precision: number) {
     return Math.ceil(amount / precision) * precision;
+  }
+
+  createDragSelectEvent(segment: WeekViewHourSegment) {
+    const dragToSelectEvent: CalendarEvent = {
+      id: this.events.length,
+      title: 'New event: ' + this.events.length.toString(),
+      start: segment.date,
+      meta: {
+        tmpEvent: true,
+      },
+    };
+    this.events = [...this.events, dragToSelectEvent];
+    return dragToSelectEvent;
   }
 }
