77d2cacd7ed13e08a60e00afa0d445c3cf4506d1
Julian Holfeld <julian@uni-kassel.de>
2022-06-12 08:37:05 +0200

    feat: add eslint rules

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -20,5 +20,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "quotes": [2, "single", {"avoidEscape": true}],
+    "comma-dangle": [2, "always-multiline"],
   },
 };
