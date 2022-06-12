15f5274f58d0f66dad1a6cf050314bc8a708998a
Julian Holfeld <julian@uni-kassel.de>
2022-06-12 08:06:26 +0200

    feat: add class-transformer with global pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,4 @@
+import {ValidationPipe} from '@nestjs/common';
 import {NestFactory} from '@nestjs/core';
 import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 
@@ -6,6 +7,7 @@ import {AppModule} from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
+    app.useGlobalPipes(new ValidationPipe({transform: true, whitelist: true}));
 
     const config = new DocumentBuilder()
         .setTitle('Apollusia')
