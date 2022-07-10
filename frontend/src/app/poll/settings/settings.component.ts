db84777d29747095b8b13d741a6540810226d415
Julian Holfeld <julian@uni-kassel.de>
2022-07-10 16:40:59 +0200

    feat: add options to settings-form

--- frontend/src/app/poll/settings/settings.component.ts
+++ frontend/src/app/poll/settings/settings.component.ts
@@ -7,10 +7,17 @@ import {FormControl, FormGroup} from '@angular/forms';
   styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit {
+  minDate = new Date();
+
+  // FIXME: fetch from backend and initialize form
   settingsForm = new FormGroup({
     maxParticipants: new FormControl(false),
     maxParticipantsInput: new FormControl('1'),
     allowMaybe: new FormControl(false),
+    allowEdit: new FormControl(false),
+    allowAnonymous: new FormControl(false),
+    deadline: new FormControl(false),
+    deadlineInput: new FormControl('2022-07-13 00:00'),
   });
 
   constructor() {
