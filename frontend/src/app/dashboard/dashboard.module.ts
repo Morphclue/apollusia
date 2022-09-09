e1223b0926e299d9590b098a65a72da65a075d0c
Julian Holfeld <julian@uni-kassel.de>
2022-09-09 09:55:50 +0200

    feat: generate token component

--- frontend/src/app/dashboard/dashboard.module.ts
+++ frontend/src/app/dashboard/dashboard.module.ts
@@ -1,22 +1,26 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HttpClientModule} from "@angular/common/http";
+import {NgbTooltipModule} from '@ng-bootstrap/ng-bootstrap';
 
 import {DashboardRoutingModule} from './dashboard-routing.module';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {CardComponent} from './card/card.component';
 import {TruncatePipe} from '../pipes/truncate.pipe';
+import {TokenComponent} from './token/token.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
     CardComponent,
     TruncatePipe,
+    TokenComponent,
   ],
   imports: [
     CommonModule,
     DashboardRoutingModule,
     HttpClientModule,
+    NgbTooltipModule,
   ],
 })
 export class DashboardModule {
