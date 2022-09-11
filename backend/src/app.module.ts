655fc8506f82d94d0af8664f1dd86c393e5d2861
Julian Holfeld <julian@uni-kassel.de>
2022-09-11 13:21:08 +0200

    feat: nest generate token module/service/controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,11 +4,13 @@ import {MongooseModule} from '@nestjs/mongoose';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {PollModule} from './poll/poll.module';
+import {TokenModule} from './token/token.module';
 
 @Module({
     imports: [
         MongooseModule.forRoot('mongodb://localhost/nest'),
         PollModule,
+        TokenModule,
     ],
     controllers: [AppController],
     providers: [AppService],
