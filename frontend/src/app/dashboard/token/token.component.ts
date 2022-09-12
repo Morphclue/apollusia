02caccc1d64021aaccf530084c0d1f99f5652947
Julian Holfeld <julian@uni-kassel.de>
2022-09-12 13:47:08 +0200

    feat: save token to input

--- frontend/src/app/dashboard/token/token.component.ts
+++ frontend/src/app/dashboard/token/token.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 
+import {TokenService} from './token.service';
+
 @Component({
   selector: 'app-token',
   templateUrl: './token.component.html',
@@ -9,10 +11,13 @@ export class TokenComponent implements OnInit {
   input: string = '';
   visible: boolean = false;
 
-  constructor() {
+  constructor(
+    private tokenService: TokenService,
+  ) {
   }
 
   ngOnInit(): void {
+    this.input = this.tokenService.getToken();
   }
 
   copyToClipboard() {
