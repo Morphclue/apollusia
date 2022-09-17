1c8d31f26650e37efa9dd6bafb8f1cc5122eb869
Julian Holfeld <julian@uni-kassel.de>
2022-09-17 15:47:37 +0200

    feat: generate feature-component

--- frontend/src/app/about/about.module.ts
+++ frontend/src/app/about/about.module.ts
@@ -5,9 +5,10 @@ import {CountUpModule} from 'ngx-countup';
 import {AboutRoutingModule} from './about-routing.module';
 import {AboutComponent} from './about/about.component';
 import {StatisticsComponent} from './statistics/statistics.component';
+import { FeaturesComponent } from './features/features.component';
 
 @NgModule({
-  declarations: [AboutComponent, StatisticsComponent],
+  declarations: [AboutComponent, StatisticsComponent, FeaturesComponent],
   imports: [
     CommonModule,
     AboutRoutingModule,
