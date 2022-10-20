286691b36c40f72ab44bc88e3ac980253eca34f7
Julian Holfeld <julian@uni-kassel.de>
2022-10-20 08:02:22 +0200

    feat: configure MailerModule with ConfigModule

--- backend/src/mail/mail.module.ts
+++ backend/src/mail/mail.module.ts
@@ -1,11 +1,36 @@
 import {Module} from '@nestjs/common';
+import {ConfigModule, ConfigService} from '@nestjs/config';
+import {MailerModule} from '@nestjs-modules/mailer';
+import {HandlebarsAdapter} from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import {join} from 'path';
 
 import {MailService} from './mail/mail.service';
 
 @Module({
-    imports: [
-        // TODO: Add and configure mailer module
-    ],
+    imports: [MailerModule.forRootAsync({
+        imports: [ConfigModule],
+        useFactory: async (config: ConfigService) => ({
+            transport: {
+                host: config.get('EMAIL_HOST'),
+                secure: false,
+                auth: {
+                    user: config.get('EMAIL_USER'),
+                    pass: config.get('EMAIL_PASSWORD'),
+                },
+            },
+            defaults: {
+                from: '<sendgrid_from_email_address>',
+            },
+            template: {
+                dir: join(__dirname, './templates'),
+                adapter: new HandlebarsAdapter(),
+                options: {
+                    strict: true,
+                },
+            },
+        }),
+        inject: [ConfigService],
+    }), ConfigModule.forRoot()],
     providers: [MailService],
     exports: [MailService],
 })
