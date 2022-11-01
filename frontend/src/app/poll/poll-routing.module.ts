e897712a166fc357e8c393726840c67a547a62f9
Adrian Kunz <clashsoft@hotmail.com>
2022-11-01 12:11:40 +0100

    fix(frontend): New modal titles

--- frontend/src/app/poll/poll-routing.module.ts
+++ frontend/src/app/poll/poll-routing.module.ts
@@ -1,18 +1,17 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {CustomDefinitionModalComponent, PostponeModalComponent} from '../modals';
+import {AutofillModalComponent, PostponeModalComponent} from '../modals';
 import {ChooseDateComponent} from './choose-date/choose-date.component';
 import {ChooseEventsComponent} from './choose-events/choose-events.component';
 import {CreateEditPollComponent} from './create-poll/create-edit-poll.component';
-import {AutofillModalComponent, PostponeModalComponent} from '../modals';
 
 const routes: Routes = [
   {path: 'create', component: CreateEditPollComponent, title: 'Create Poll'},
   {path: ':id', component: CreateEditPollComponent, title: 'Edit Poll'},
   {
     path: ':id/date', component: ChooseDateComponent, title: 'Choose Dates', children: [
-      {path: 'autofill', component: AutofillModalComponent},
-      {path: 'postpone', component: PostponeModalComponent},
+      {path: 'autofill', component: AutofillModalComponent, title: 'Autofill'},
+      {path: 'postpone', component: PostponeModalComponent, title: 'Postpone'},
     ],
   },
   {path: ':id/participate', component: ChooseEventsComponent, title: 'Poll'},
