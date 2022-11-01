225b96c025815593b6afa4671cfc503770b9b514
Julian Holfeld <julian@uni-kassel.de>
2022-11-01 14:48:53 +0100

    feat: add  | Apollusia to title

--- frontend/src/app/poll/poll-routing.module.ts
+++ frontend/src/app/poll/poll-routing.module.ts
@@ -1,20 +1,21 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+
 import {AutofillModalComponent, PostponeModalComponent} from '../modals';
 import {ChooseDateComponent} from './choose-date/choose-date.component';
 import {ChooseEventsComponent} from './choose-events/choose-events.component';
 import {CreateEditPollComponent} from './create-poll/create-edit-poll.component';
 
 const routes: Routes = [
-  {path: 'create', component: CreateEditPollComponent, title: 'Create Poll'},
-  {path: ':id', component: CreateEditPollComponent, title: 'Edit Poll'},
+  {path: 'create', component: CreateEditPollComponent, title: 'Create Poll | Apollusia'},
+  {path: ':id', component: CreateEditPollComponent, title: 'Edit Poll | Apollusia'},
   {
-    path: ':id/date', component: ChooseDateComponent, title: 'Choose Dates', children: [
-      {path: 'autofill', component: AutofillModalComponent, title: 'Autofill'},
-      {path: 'postpone', component: PostponeModalComponent, title: 'Postpone'},
+    path: ':id/date', component: ChooseDateComponent, title: 'Choose Dates | Apollusia', children: [
+      {path: 'autofill', component: AutofillModalComponent, title: 'Autofill | Apollusia'},
+      {path: 'postpone', component: PostponeModalComponent, title: 'Postpone | Apollusia'},
     ],
   },
-  {path: ':id/participate', component: ChooseEventsComponent, title: 'Poll'},
+  {path: ':id/participate', component: ChooseEventsComponent, title: 'Poll | Apollusia'},
 ];
 
 @NgModule({
