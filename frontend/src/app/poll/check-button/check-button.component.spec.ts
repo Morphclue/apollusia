29de9caa70f3a980279fa4552c1ee286d3d0873a
Julian Holfeld <julian@uni-kassel.de>
2022-11-05 09:40:01 +0100

    refactor: formatting

--- frontend/src/app/poll/check-button/check-button.component.spec.ts
+++ frontend/src/app/poll/check-button/check-button.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { CheckButtonComponent } from './check-button.component';
+import {CheckButtonComponent} from './check-button.component';
 
 describe('CheckButtonComponent', () => {
   let component: CheckButtonComponent;
@@ -8,9 +8,9 @@ describe('CheckButtonComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CheckButtonComponent ]
+      declarations: [CheckButtonComponent],
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(CheckButtonComponent);
     component = fixture.componentInstance;
