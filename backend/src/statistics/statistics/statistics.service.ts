66cdff3d0e5eca7781c265de7b72db8e764b9776
Adrian Kunz <clashsoft@hotmail.com>
2022-11-07 16:07:50 +0100

    feat(backend): Unique users statistic

--- backend/src/statistics/statistics/statistics.service.ts
+++ backend/src/statistics/statistics/statistics.service.ts
@@ -1,24 +1,39 @@
 import {Injectable} from '@nestjs/common';
 import {InjectModel} from '@nestjs/mongoose';
 import {Model} from 'mongoose';
+import {StatisticsDto} from '../../dto';
 
 import {Participant, Poll, PollEvent} from '../../schema';
-import {StatisticsDto} from '../../dto';
 
 @Injectable()
 export class StatisticsService {
-    constructor(
-        @InjectModel(Poll.name) private pollModel: Model<Poll>,
-        @InjectModel(PollEvent.name) private pollEventModel: Model<PollEvent>,
-        @InjectModel(Participant.name) private participantModel: Model<Participant>,
-    ) {
-    }
+  constructor(
+    @InjectModel(Poll.name) private pollModel: Model<Poll>,
+    @InjectModel(PollEvent.name) private pollEventModel: Model<PollEvent>,
+    @InjectModel(Participant.name) private participantModel: Model<Participant>,
+  ) {
+  }
 
-    async getStats() {
-        return {
-            polls: await this.pollModel.countDocuments().exec(),
-            pollEvents: await this.pollEventModel.countDocuments().exec(),
-            participants: await this.participantModel.countDocuments().exec(),
-        } as StatisticsDto;
-    }
+  async getStats(): Promise<StatisticsDto> {
+    const [
+      polls,
+      pollEvents,
+      participants,
+      pollTokens,
+      participantTokens,
+    ] = await Promise.all([
+      this.pollModel.countDocuments().exec(),
+      this.pollEventModel.countDocuments().exec(),
+      this.participantModel.countDocuments().exec(),
+      this.pollModel.distinct('token').exec(),
+      this.participantModel.distinct('token').exec(),
+    ]);
+    return {
+      polls,
+      pollEvents,
+      participants,
+      // TODO this may be inefficient with large number of users
+      users: new Set([...pollTokens, ...participantTokens]).size,
+    };
+  }
 }
