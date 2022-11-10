b3698dd9c9b5c50b73d3d16a1c5eb993d473d899
Adrian Kunz <clashsoft@hotmail.com>
2022-11-10 13:35:05 +0100

    feat(backend): Markdown description in email

--- backend/src/mail/mail/mail.service.ts
+++ backend/src/mail/mail/mail.service.ts
@@ -1,10 +1,19 @@
 import {MailerService} from '@nestjs-modules/mailer';
 import {Injectable} from '@nestjs/common';
+import * as MarkdownIt from 'markdown-it';
+import * as Handlebars from 'handlebars';
 import {Participant, Poll} from '../../schema';
 
 @Injectable()
 export class MailService {
     constructor(private mailerService: MailerService) {
+        const markdown = new MarkdownIt({
+            breaks: true,
+            linkify: true,
+        });
+        Handlebars.registerHelper('markdown', function (context) {
+            return new Handlebars.SafeString(markdown.render(context));
+        });
     }
 
     async sendMail(poll: Poll, participant: Participant, appointments: string[]) {
