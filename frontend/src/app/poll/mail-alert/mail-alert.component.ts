fb449713859b44c88cb717b2a8a6a7de5d9eb5e8
Adrian Kunz <clashsoft@hotmail.com>
2022-11-12 10:35:05 +0100

    fix(frontend): Configure mail link in mail-alert component

--- frontend/src/app/poll/mail-alert/mail-alert.component.ts
+++ frontend/src/app/poll/mail-alert/mail-alert.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-mail-alert',
@@ -6,4 +7,8 @@ import {Component} from '@angular/core';
   styleUrls: ['./mail-alert.component.scss'],
 })
 export class MailAlertComponent {
+  constructor(
+    public route: ActivatedRoute,
+  ) {
+  }
 }
