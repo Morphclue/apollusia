d07bb164e42482b83f2e04b07c0b0daa434097c3
Julian Holfeld <julian@uni-kassel.de>
2022-11-28 13:50:28 +0100

    test: pollStub and regenerate token for poll

--- backend/src/token/token/token.service.spec.ts
+++ backend/src/token/token/token.service.spec.ts
@@ -4,9 +4,12 @@ import {Test, TestingModule} from '@nestjs/testing';
 import {Poll, PollSchema} from '../../schema';
 import {closeMongoConnection, rootMongooseTestModule} from '../../utils/mongo-util';
 import {TokenService} from './token.service';
+import {Model} from 'mongoose';
+import {PollStub} from '../../../test/stubs/PollStub';
 
 describe('TokenService', () => {
     let service: TokenService;
+    let pollModel: Model<Poll>;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
@@ -17,6 +20,7 @@ describe('TokenService', () => {
             providers: [TokenService],
         }).compile();
 
+        pollModel = module.get('PollModel');
         service = module.get<TokenService>(TokenService);
     });
 
@@ -33,6 +37,15 @@ describe('TokenService', () => {
         const token = await service.generateToken();
         const newToken = await service.regenerateToken(token.token);
         expect(newToken).toBeDefined();
+    });
+
+    it('should regenerate a token for a poll', async () => {
+        const pollStub = PollStub();
+        await (new pollModel(pollStub)).save();
+        const newToken = await service.regenerateToken(pollStub.adminToken);
+        const newPoll = await pollModel.findOne({adminToken: newToken.token}).exec();
+        expect(newPoll).toBeDefined();
+        expect(newPoll.adminToken).not.toEqual(pollStub.adminToken);
     });
 
     afterAll(async () => {
