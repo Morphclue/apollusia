b448264e049c59b10971a1b941c9d155d25126c0
Julian Holfeld <julian@uni-kassel.de>
2022-11-29 13:24:47 +0100

    test: add PollEventStub and ParticipantStub

---
+++ backend/test/stubs/ParticipantStub.ts
@@ -0,0 +1,14 @@
+import {Types} from 'mongoose';
+
+import {Participant} from '../../src/schema';
+
+export const ParticipantStub = (): Participant => {
+    return {
+        _id: new Types.ObjectId('6385f9a2af23127d1e023759'),
+        poll: new Types.ObjectId('5f1f9b9b9b9b9b9b9b9b9b9b'),
+        name: 'Administrator',
+        participation: [],
+        indeterminateParticipation: [],
+        token: '619b3a00-2dc3-48f1-8b3d-50386a91a559',
+    };
+};
