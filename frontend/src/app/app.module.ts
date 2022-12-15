e82bba63da0ac5509c9e40896aa490810acb2982
Adrian Kunz <clashsoft@hotmail.com>
2022-12-15 12:59:01 +0100

    feat(frontend): Set up push notifications

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -29,7 +29,7 @@ import { environment } from '../environments/environment';
     AboutModule,
     CoreModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
+      enabled: true,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   backendURL: 'https://apollusia.uniks.de/api/v1',
+  vapidPublicKey: 'BDURWmqCQRdvAguZ6qAH6pDgPn1N5ksSn3s2fPJRBGOqm1l83kr8N8MK__3P_v-SCEHCUCvQI4W4jEuBS67P0HU',
 };
