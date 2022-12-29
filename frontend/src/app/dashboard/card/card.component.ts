97f297e373f7557aabf5f6b30846c3334c656a04
Adrian Kunz <clashsoft@hotmail.com>
2022-12-29 14:40:30 +0100

    fix(frontend): Remove admin check for each poll

--- frontend/src/app/dashboard/card/card.component.ts
+++ frontend/src/app/dashboard/card/card.component.ts
@@ -1,8 +1,4 @@
-import {HttpClient} from '@angular/common/http';
-import {Component, Input, OnInit} from '@angular/core';
-
-import {environment} from '../../../environments/environment';
-import {TokenService} from '../../core/services';
+import {Component, Input} from '@angular/core';
 import {ReadPoll} from '../../model';
 
 @Component({
@@ -10,27 +6,6 @@ import {ReadPoll} from '../../model';
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
-export class CardComponent implements OnInit {
-  @Input() poll: ReadPoll | undefined;
-  isAdmin: boolean = false;
-
-  constructor(
-    private http: HttpClient,
-    private tokenService: TokenService,
-  ) {
-  }
-
-  ngOnInit(): void {
-    this.checkAdmin();
-  }
-
-  private checkAdmin() {
-    if (!this.poll) {
-      return;
-    }
-    const adminToken = this.tokenService.getToken();
-    this.http.get<boolean>(`${environment.backendURL}/poll/${this.poll._id}/admin/${adminToken}`).subscribe(isAdmin => {
-      this.isAdmin = isAdmin;
-    });
-  }
+export class CardComponent {
+  @Input() poll?: ReadPoll;
 }
