d6a515bdd8c1f2d68d6a4222456689c2ac20c3f6
Julian Holfeld <julian@uni-kassel.de>
2023-05-05 19:09:38 +0200

    test: call post- and getPoll

--- apps/backend/src/poll/poll/poll.controller.spec.ts
+++ apps/backend/src/poll/poll/poll.controller.spec.ts
@@ -1,11 +1,14 @@
 import {Test, TestingModule} from '@nestjs/testing';
 
 import {PollController} from './poll.controller';
+import {PollService} from './poll.service';
+import {PollStub} from '../../../test/stubs';
 import {rootMongooseTestModule} from '../../utils/mongo-util';
 import {PollModule} from '../poll.module';
 
 describe('PollController', () => {
     let controller: PollController;
+    let service: PollService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
@@ -16,9 +19,22 @@ describe('PollController', () => {
         }).compile();
 
         controller = module.get<PollController>(PollController);
+        service = module.get<PollService>(PollService);
     });
 
     it('should be defined', () => {
         expect(controller).toBeDefined();
+    });
+
+    it('should call postPoll', async () => {
+      jest.spyOn(service, 'postPoll')
+      await controller.postPoll(PollStub());
+      expect(service.postPoll).toHaveBeenCalled();
+    });
+
+    it('should call getPoll', async () => {
+      jest.spyOn(service, 'getPoll')
+      await controller.getPoll(PollStub()._id.toString());
+      expect(service.getPoll).toHaveBeenCalled();
     });
 });
