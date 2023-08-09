fb77cdaee9fa147459e58c73ca342bd38011bfe9
Adrian Kunz <a.kunz@uni-kassel.de>
2023-08-09 16:08:58 +0200

    feat: Add DuD-Poll feature comparison

--- apps/frontend/src/app/about/features/features.component.ts
+++ apps/frontend/src/app/about/features/features.component.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
 
-const apps = ['Apollusia', 'Doodle'] as const;
+const apps = ['Apollusia', 'Doodle', 'DuD-Poll'] as const;
 type App = typeof apps[number];
 
 interface Feature {
   title: string;
   description: string;
+  apollusiaIssue?: number;
   support: Record<App, boolean | string>;
 }
 
@@ -20,62 +21,82 @@ export class FeaturesComponent {
     {
       title: 'Anonymous participation',
       description: 'Participants can vote without entering their name',
-      support: {Apollusia: true, Doodle: true}
+      support: {Apollusia: true, Doodle: true, "DuD-Poll": true}
     },
     {
       title: 'Participation limit',
       description: 'Limit the number of participants',
-      support: {Apollusia: true, Doodle: true}
+      support: {Apollusia: true, Doodle: true, "DuD-Poll": false}
     },
     {
       title: 'Editing participation',
       description: 'Participants can edit their participation',
-      support: {Apollusia: true, Doodle: true}
+      support: {Apollusia: true, Doodle: true, "DuD-Poll": true}
     },
     {
       title: 'Deadline',
       description: 'Set a deadline for new participations',
-      support: {Apollusia: true, Doodle: 'Paid option'}
+      support: {Apollusia: true, Doodle: 'Paid option', "DuD-Poll": false}
     },
     {
       title: 'Maybe-option',
       description: 'Friendlier event selection with a Maybe option',
-      support: {Apollusia: true, Doodle: 'Not adjustable'}
+      support: {Apollusia: true, Doodle: 'Not adjustable', "DuD-Poll": 'Always'}
     },
     {
       title: 'Rich Text and Links in Description',
       description: 'Add Markdown formatted text and links to the description',
-      support: {Apollusia: true, Doodle: false}
+      support: {Apollusia: true, Doodle: false, "DuD-Poll": false}
     },
     {
       title: 'Events of varying length',
       description: 'Every event can have its own length',
-      support: {Apollusia: true, Doodle: false}
+      support: {Apollusia: true, Doodle: false, "DuD-Poll": false}
     },
     {
       title: 'Blind participation',
       description: 'Participants can\'t see other participants until they participate',
-      support: {Apollusia: true, Doodle: false}
+      support: {Apollusia: true, Doodle: false, "DuD-Poll": false}
     },
     {
       title: 'Autofill',
       description: 'Create many sequential events automatically, with breaks and on multiple days',
-      support: {Apollusia: true, Doodle: false}
+      support: {Apollusia: true, Doodle: false, "DuD-Poll": 'Repeating the time for every day is possible'}
     },
     {
       title: 'Event Notes',
       description: 'Add notes to events to provide additional information',
-      support: {Apollusia: true, Doodle: false}
+      support: {Apollusia: true, Doodle: false, "DuD-Poll": false}
     },
     {
       title: 'Cloning polls',
       description: 'Clone polls to start off with the same options',
-      support: {Apollusia: true, Doodle: false}
+      support: {Apollusia: true, Doodle: false, "DuD-Poll": false}
     },
     {
       title: 'Postponing events',
       description: 'Move all events to a later date',
-      support: {Apollusia: true, Doodle: false}
+      support: {Apollusia: true, Doodle: false, "DuD-Poll": false}
+    },
+    {
+      title: 'Invite Participants',
+      description: 'Invite participants via email or other means',
+      support: {Apollusia: 'Simple link sharing is possible', Doodle: true, 'DuD-Poll': true}
+    },
+    {
+      title: 'Access Control',
+      description: 'Restrict access to your poll',
+      support: {Apollusia: 'Only for creator, but planned. See #60', Doodle: false, 'DuD-Poll': true}
+    },
+    {
+      title: 'History',
+      description: 'See all changes to a poll',
+      support: {Apollusia: false, Doodle: false, 'DuD-Poll': true}
+    },
+    {
+      title: 'Comments',
+      description: 'Add comments to a poll',
+      support: {Apollusia: false, Doodle: false, 'DuD-Poll': true}
     },
   ];
 }
