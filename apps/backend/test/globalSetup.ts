9433f75fd77db850d87c82c3b0f65f31a1b7a333
Adrian Kunz <clashsoft@hotmail.com>
2024-02-17 20:59:56 +0100

    test(backend): Clear db on global setup

--- apps/backend/test/globalSetup.ts
+++ apps/backend/test/globalSetup.ts
@@ -1,5 +1,5 @@
 import {MongoMemoryServer} from 'mongodb-memory-server';
-import * as mongoose from 'mongoose';
+import * as mongoose from "mongoose";
 
 export = async function globalSetup() {
   // it's needed in global space, because we don't want to create a new instance every test-suite
@@ -7,10 +7,7 @@ export = async function globalSetup() {
   (global as any).__MONGOINSTANCE = instance;
   process.env.MONGO_URI = instance.getUri();
 
-  // The following is to make sure the database is clean before an test starts
-  /*
-  await mongoose.connect(process.env.MONGO_URI);
-  await mongoose.connection.db.dropDatabase();
-  await mongoose.disconnect();
-  */
+  const conn = mongoose.createConnection(process.env.MONGO_URI);
+  await conn.dropDatabase();
+  await conn.close();
 };
