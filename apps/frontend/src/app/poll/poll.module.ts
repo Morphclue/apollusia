e179259f04b79fad712eb6c6b01bd0149d5d5c08
Adrian Kunz <clashsoft@hotmail.com>
2024-02-25 16:04:29 +0100

    feat(frontend): Add event list view

--- apps/frontend/src/app/poll/poll.module.ts
+++ apps/frontend/src/app/poll/poll.module.ts
@@ -1,24 +1,25 @@
-import {CommonModule} from '@angular/common';
-import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {ModalModule, ToastModule} from '@mean-stream/ngbx';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {CalendarModule, DateAdapter} from 'angular-calendar';
-import {adapterFactory} from 'angular-calendar/date-adapters/date-fns';
-import {FlatpickrModule} from 'angularx-flatpickr';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ModalModule, ToastModule } from '@mean-stream/ngbx';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { FlatpickrModule } from 'angularx-flatpickr';
 
-import {CheckButtonComponent} from './check-button/check-button.component';
-import {ChooseDateComponent} from './choose-date/choose-date.component';
-import {ChooseEventsComponent} from './choose-events/choose-events.component';
-import {CreateEditPollComponent} from './create-poll/create-edit-poll.component';
-import {EventHeadComponent} from './event-head/event-head.component';
-import {MailAlertComponent} from './mail-alert/mail-alert.component';
-import {PollRoutingModule} from './poll-routing.module';
-import {ChooseDateService} from './services/choose-date.service';
-import {TableComponent} from "./table/table.component";
-import {CoreModule} from '../core/core.module';
-import {AutofillModalComponent, PostponeModalComponent} from '../modals';
-import {SomePipe} from '../pipes';
+import { CheckButtonComponent } from './check-button/check-button.component';
+import { ChooseDateComponent } from './choose-date/choose-date.component';
+import { ChooseEventsComponent } from './choose-events/choose-events.component';
+import { CreateEditPollComponent } from './create-poll/create-edit-poll.component';
+import { EventHeadComponent } from './event-head/event-head.component';
+import { MailAlertComponent } from './mail-alert/mail-alert.component';
+import { PollRoutingModule } from './poll-routing.module';
+import { ChooseDateService } from './services/choose-date.service';
+import { TableComponent } from './table/table.component';
+import { CoreModule } from '../core/core.module';
+import { AutofillModalComponent, PostponeModalComponent } from '../modals';
+import { SomePipe } from '../pipes';
+import { EventListComponent } from './event-list/event-list.component';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import {SomePipe} from '../pipes';
     CheckButtonComponent,
     MailAlertComponent,
     TableComponent,
+    EventListComponent,
   ],
   imports: [
     CommonModule,
@@ -48,9 +50,6 @@ import {SomePipe} from '../pipes';
     }),
     FlatpickrModule.forRoot(),
   ],
-  providers: [
-    ChooseDateService,
-  ],
+  providers: [ChooseDateService],
 })
-export class PollModule {
-}
+export class PollModule {}
