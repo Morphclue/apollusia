fe83a48034685c5a3ad93d0180844cc855313b4f
Adrian Kunz <clashsoft@hotmail.com>
2024-05-17 22:23:17 +0200

    style(backend): Reformat PushService

--- apps/backend/src/push/push.service.ts
+++ apps/backend/src/push/push.service.ts
@@ -10,36 +10,36 @@ export class PushService {
 
   config?: PushConfigDto;
 
-    constructor(
-        config: ConfigService,
-    ) {
-      const publicKey = config.get('VAPID_PUBLIC_KEY');
-      const privateKey = config.get('VAPID_PRIVATE_KEY');
-      const emailSender = config.get('EMAIL_FROM');
-      if (publicKey && privateKey && emailSender) {
-        webpush.setVapidDetails('mailto:' + emailSender, publicKey, privateKey);
+  constructor(
+    config: ConfigService,
+  ) {
+    const publicKey = config.get('VAPID_PUBLIC_KEY');
+    const privateKey = config.get('VAPID_PRIVATE_KEY');
+    const emailSender = config.get('EMAIL_FROM');
+    if (publicKey && privateKey && emailSender) {
+      webpush.setVapidDetails('mailto:' + emailSender, publicKey, privateKey);
 
-        this.config = {
-          vapidPublicKey: publicKey,
-        };
-      } else {
-        this.logger.warn('VAPID keys not set. Push notifications will not work.');
-      }
+      this.config = {
+        vapidPublicKey: publicKey,
+      };
+    } else {
+      this.logger.warn('VAPID keys not set. Push notifications will not work.');
     }
+  }
 
-    async send(sub: PushSubscription, title: string, body: string, url: string) {
-        const payload = {
-            notification: {
-                title,
-                body,
-                // icon: 'assets/main-page-logo-small-hat.png',
-                data: {
-                    onActionClick: {
-                        'default': {operation: 'openWindow', url},
-                    },
-                },
-            },
-        };
-        await webpush.sendNotification(sub, JSON.stringify(payload));
-    }
+  async send(sub: PushSubscription, title: string, body: string, url: string) {
+    const payload = {
+      notification: {
+        title,
+        body,
+        // icon: 'assets/main-page-logo-small-hat.png',
+        data: {
+          onActionClick: {
+            'default': {operation: 'openWindow', url},
+          },
+        },
+      },
+    };
+    await webpush.sendNotification(sub, JSON.stringify(payload));
+  }
 }
