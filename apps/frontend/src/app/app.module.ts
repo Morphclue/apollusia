45049bc8dc47f421bb9d454914ad3dc5dd8649a3
Adrian Kunz <a.kunz@uni-kassel.de>
2024-07-23 14:16:25 +0200

    fix(frontend): Try to fix SSR

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -17,14 +17,18 @@ import {LegalModule} from './legal/legal.module';
 import {environment} from '../environments/environment';
 
 function initializeKeycloak(keycloak: KeycloakService) {
-  return () =>
-    keycloak.init({
+  return async () => {
+    if (!globalThis.window) {
+      return true;
+    }
+    return keycloak.init({
       config: environment.keycloak,
       initOptions: {
         onLoad: 'check-sso',
-        silentCheckSsoRedirectUri: window.location.origin + '/assets/silent-check-sso.html'
-      }
+        silentCheckSsoRedirectUri: window.location.origin + '/assets/silent-check-sso.html',
+      },
     });
+  };
 }
 
 @NgModule({
