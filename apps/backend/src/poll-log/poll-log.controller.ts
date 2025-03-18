ed7fc02e4a80902d22424d30399ddde6d4f42f16
Adrian Kunz <clashsoft@hotmail.com>
2025-03-18 22:17:45 +0100

    feat(backend): Paginate poll logs

--- apps/backend/src/poll-log/poll-log.controller.ts
+++ apps/backend/src/poll-log/poll-log.controller.ts
@@ -2,7 +2,7 @@ import {PollLog} from '@apollusia/types';
 import {CreatePollLogDto} from '@apollusia/types/lib/dto/poll-log.dto';
 import {Auth, AuthUser, UserToken} from '@mean-stream/nestx/auth';
 import {ObjectIdPipe} from '@mean-stream/nestx/ref';
-import {Body, Controller, Get, Param, Post} from '@nestjs/common';
+import {Body, Controller, Get, Param, Post, Query} from '@nestjs/common';
 import {Types} from 'mongoose';
 
 import {PollLogService} from './poll-log.service';
@@ -17,8 +17,14 @@ export class PollLogController {
   @Get()
   async getEvents(
     @Param('poll', ObjectIdPipe) poll: Types.ObjectId,
+    @Query('createdBefore') createdBefore?: string,
   ): Promise<PollLog[]> {
-    return this.pollLogService.findAll({poll});
+    return this.pollLogService.findAll({
+      poll,
+      createdAt: createdBefore ? {$lt: new Date(createdBefore)} : undefined,
+    }, {
+      limit: 100,
+    });
   }
 
   @Post()
