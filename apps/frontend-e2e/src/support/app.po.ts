d58d73d0ea88ca8c88ee098976f674c6d0788040
Julian Holfeld <morphclue@gmail.com>
2025-06-07 14:25:31 +0200

    refactor: separate functions

--- apps/frontend-e2e/src/support/app.po.ts
+++ apps/frontend-e2e/src/support/app.po.ts
@@ -3,4 +3,32 @@ export class AppPage{
     cy.get('app-cookie-banner').should('be.visible');
     cy.get('app-cookie-banner .btn-link').click();
   }
+
+  expectEmptyDashboardVisible(){
+    cy.get('app-navbar').should('be.visible');
+    cy.get('app-dashboard').should('be.visible');
+    cy.get('app-dashboard li').contains('There is nothing here.');
+    cy.get('app-navbar').should('be.visible');
+  }
+
+  openCreatePollPage(){
+    cy.get('app-navbar .btn-primary').contains('Create Poll').click();
+    cy.url().should('include', '/poll/create');
+    cy.get('app-create-edit-poll').should('be.visible');
+    cy.get('button[type="submit"]').should('be.disabled')
+  }
+
+  fillPollForm() {
+    cy.get('input[id="title"]').type('My Poll');
+    cy.get('button[type="submit"]').should('not.be.disabled')
+    cy.get('textarea[id="description"]').type('This is short description');
+    cy.get('.input-group-text').should('have.class', 'bi-geo');
+    cy.get('input[id="location"]').type('Discord');
+    cy.get('.input-group-text').should('have.class', 'bi-discord');
+    cy.get('input[id="deadline"]').type('2222-10-15');
+    cy.get('input[id="time"]').type('04:20');
+    cy.get('fieldset div div').contains('Group').click();
+    cy.get('button[aria-controls="collapseExample"]')
+      .should('have.attr', 'aria-expanded', 'false');
+  }
 }
