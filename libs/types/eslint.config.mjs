19898298b5f79e8677cba688f74ec97122a7d179
Julian Holfeld <morphclue@gmail.com>
2025-06-12 08:35:55 +0200

    refactor: remove boilerplate in libs

--- libs/types/eslint.config.mjs
+++ libs/types/eslint.config.mjs
@@ -7,17 +7,6 @@ export default [
   ...baseConfig,
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-    // Override or add rules here
-    rules: {},
-  },
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    // Override or add rules here
-    rules: {},
-  },
-  {
-    files: ['**/*.js', '**/*.jsx'],
-    // Override or add rules here
     rules: {},
   },
 ];
