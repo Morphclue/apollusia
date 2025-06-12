162113075f8e348e6f5055eb8fa8a894b5450a0b
Julian Holfeld <morphclue@gmail.com>
2025-06-12 16:16:18 +0200

    feat: use types for mocks

--- apps/frontend-e2e/src/support/mocks/mock-events.ts
+++ apps/frontend-e2e/src/support/mocks/mock-events.ts
@@ -1,18 +1,19 @@
-export const mockEvents = [
+import { ReadPollEventDto } from '@apollusia/types';
+import { Types } from 'mongoose';
+
+export const mockEvents: ReadPollEventDto[] = [
   {
-    "_id": "6844785ed2866d622a3a2ea4",
-    "poll": "684467b0eb92799b3c597357",
-    "start": "2025-06-08T20:30:00.000Z",
-    "end": "2025-06-08T20:45:00.000Z",
-    "__v": 0,
-    "participants": 0
+    _id: new Types.ObjectId('6844785ed2866d622a3a2ea4'),
+    poll: new Types.ObjectId('684467b0eb92799b3c597357'),
+    start: new Date('3025-06-08T20:30:00.000Z'),
+    end: new Date('3025-06-08T20:45:00.000Z'),
+    participants: 0,
   },
   {
-    "_id": "6844785ed2866d622a3a2ea3",
-    "poll": "684467b0eb92799b3c597357",
-    "start": "2025-06-08T23:30:00.000Z",
-    "end": "2025-06-08T23:45:00.000Z",
-    "__v": 0,
-    "participants": 0
-  }
-]
+    _id: new Types.ObjectId('6844785ed2866d622a3a2ea3'),
+    poll: new Types.ObjectId('684467b0eb92799b3c597357'),
+    start: new Date('3025-06-08T23:30:00.000Z'),
+    end: new Date('3025-06-08T23:45:00.000Z'),
+    participants: 0,
+  },
+];
--- apps/frontend-e2e/src/support/mocks/mock-poll.ts
+++ apps/frontend-e2e/src/support/mocks/mock-poll.ts
@@ -1,23 +1,25 @@
-export const mockPoll = {
-  "title": "My Poll",
-  "description": "This is short description",
-  "location": "Discord",
-  "timeZone": "Europe/Berlin",
-  "adminMail": false,
-  "adminPush": false,
-  "settings": {
-    "deadline": "2222-10-15T04:20:00.000Z",
-    "allowMaybe": true,
-    "allowEdit": true,
-    "anonymous": false,
-    "allowComments": true,
-    "logHistory": true,
-    "showResult": "immediately"
+import { ReadPollDto, ShowResultOptions } from '@apollusia/types';
+import { Types } from 'mongoose';
+
+export const mockPoll: ReadPollDto = {
+  title: 'My Poll',
+  description: 'This is short description',
+  location: 'Discord',
+  timeZone: 'Europe/Berlin',
+  adminMail: false,
+  adminPush: false,
+  settings: {
+    deadline: new Date('3333-10-15T04:20:00.000Z'),
+    allowMaybe: true,
+    allowEdit: true,
+    anonymous: false,
+    allowComments: true,
+    logHistory: true,
+    showResult: 'immediately' as ShowResultOptions,
   },
-  "bookedEvents": {},
-  "_id": "684467b0eb92799b3c597357",
-  "createdAt": "2025-06-07T16:00:00.238Z",
-  "updatedAt": "2025-06-07T16:00:00.238Z",
-  "__v": 0,
-  "id": "aERnsOuSeZs8WXNX"
+  bookedEvents: {},
+  _id:  new Types.ObjectId('684467b0eb92799b3c597357'),
+  createdAt: new Date('3025-06-07T16:00:00.238Z'),
+  updatedAt: new Date('3025-06-07T16:00:00.238Z'),
+  id: 'aERnsOuSeZs8WXNX'
 }
