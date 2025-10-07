eb1acac44ec7d9e52e14ce7d59e68fc7c711c228
Julian Holfeld <morphclue@gmail.com>
2025-10-07 17:40:44 +0200

    fix(fe-e2e): eslint.config.mjs

--- apps/frontend-e2e/eslint.config.mjs
+++ apps/frontend-e2e/eslint.config.mjs
@@ -1,19 +1,21 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import js from '@eslint/js';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import cypress from 'eslint-plugin-cypress';
 
 import baseConfig from '../../eslint.config.mjs';
-
-const compat = new FlatCompat({
-  baseDirectory: dirname(fileURLToPath(import.meta.url)),
-  recommendedConfig: js.configs.recommended,
-});
 
 export default [
   {
     ignores: ['**/dist'],
   },
   ...baseConfig,
-  ...compat.extends('plugin:cypress/recommended'),
+  {
+    files: ['**/*.ts', '**/*.js'],
+    languageOptions: {
+      globals: {
+        Cypress: 'readonly',
+        cy: 'readonly',
+      },
+    },
+    plugins: { cypress },
+    ...cypress.configs.recommended,
+  },
 ];
