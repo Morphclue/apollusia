81ae6127fed888002f63a0deb979012106180dd9
Julian Holfeld <morphclue@gmail.com>
2025-10-19 19:15:05 +0200

    fix: deprecated platformBrowserDynamic

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -1,7 +1,7 @@
 /// <reference types="@angular/localize" />
 
 import {enableProdMode} from '@angular/core';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {platformBrowser} from '@angular/platform-browser';
 
 import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
@@ -11,7 +11,7 @@ if (environment.production) {
 }
 
 function bootstrap() {
-  platformBrowserDynamic().bootstrapModule(AppModule)
+  platformBrowser().bootstrapModule(AppModule)
     .catch(err => console.error(err));
 }
 
