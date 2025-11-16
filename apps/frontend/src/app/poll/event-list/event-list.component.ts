2962bd5b1697ce6f1d3906b93948a24775a8f521
Adrian Kunz <a.kunz@uni-kassel.de>
2025-11-16 17:55:48 +0100

    feat: Group event list by date to reduce clutter

--- apps/frontend/src/app/poll/event-list/event-list.component.ts
+++ apps/frontend/src/app/poll/event-list/event-list.component.ts
@@ -1,5 +1,5 @@
 import {DatePipe} from '@angular/common';
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {NgbTooltip} from '@ng-bootstrap/ng-bootstrap';
 
@@ -17,11 +17,27 @@ import {ParticipantInfoComponent} from '../participant-info/participant-info.com
     FormsModule,
   ],
 })
-export class EventListComponent {
+export class EventListComponent implements OnInit {
   @Input() poll!: ReadPoll;
   @Input() pollEvents!: ReadPollEvent[];
   @Input() participants!: Participant[];
   @Input() bestOption!: number;
 
   protected showNoVotes = false;
+  eventsGroupedByDate: [Date, ReadPollEvent[]][] = [];
+
+  ngOnInit() {
+    const groupedEvents: Map<number, ReadPollEvent[]> = new Map();
+    for (const event of this.pollEvents) {
+      const eventDate = new Date(event.start).setHours(0, 0, 0, 0); // Normalize to the start of the day
+      if (!groupedEvents.has(eventDate)) {
+        groupedEvents.set(eventDate, []);
+      }
+      groupedEvents.get(eventDate)!.push(event);
+    }
+    this.eventsGroupedByDate = Array.from(groupedEvents.entries())
+      .sort((a, b) => +a[0] - +b[0])
+      .map(([dateMs, events]) => [new Date(dateMs), events])
+    ;
+  }
 }
