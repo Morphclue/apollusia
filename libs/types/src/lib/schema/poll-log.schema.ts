e9d138f6481dc8c70cff1cfc69046d8aac3cb48b
Adrian Kunz <a.kunz@uni-kassel.de>
2026-05-09 17:19:37 +0200

    fix(types): Revert removal of validation decorators

--- libs/types/src/lib/schema/poll-log.schema.ts
+++ libs/types/src/lib/schema/poll-log.schema.ts
@@ -1,6 +1,8 @@
 import {Ref} from '@mean-stream/nestx/ref';
 import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
 import {ApiProperty} from '@nestjs/swagger';
+import {Type} from 'class-transformer';
+import {IsNotEmpty, IsOptional, IsString, IsUUID, ValidateNested} from 'class-validator';
 import {SchemaTypes, Types} from 'mongoose';
 
 import {Participant} from './participant.schema';
@@ -8,14 +10,20 @@ import {Poll} from './poll.schema';
 
 export class Comment {
   @Prop()
+  @IsString()
+  @IsNotEmpty()
   name: string;
 
   @Prop()
+  @IsString()
+  @IsNotEmpty()
   body: string;
 }
 
 export class ParticipantLog {
   @Prop()
+  @IsOptional()
+  @IsString()
   name?: string;
 
   @Ref(Participant.name)
@@ -54,6 +62,8 @@ export class PollLog {
 
   @ApiProperty({format: 'uuid'})
   @Prop({required: false, type: SchemaTypes.UUID, transform: (v: object) => v.toString()})
+  @IsOptional()
+  @IsUUID()
   createdBy?: string;
 
   @Ref(Poll.name, {index: 1})
@@ -63,6 +73,19 @@ export class PollLog {
   type: 'comment' | 'participant.created' | 'events.changed' | 'poll.booked';
 
   @Prop({type: Object})
+  @ValidateNested()
+  @Type(() => Object, {
+    keepDiscriminatorProperty: true,
+    discriminator: {
+      property: 'type',
+      subTypes: [
+        {name: 'comment', value: Comment},
+        {name: 'participant.created', value: ParticipantLog},
+        {name: 'events.changed', value: EventsChangedLog},
+        {name: 'poll.booked', value: PollBookedLog},
+      ],
+    },
+  })
   data: Comment | ParticipantLog | EventsChangedLog | PollBookedLog;
 }
 
