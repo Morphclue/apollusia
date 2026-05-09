3066a5ffe959c76df3f0b0580062c7f0e6ec02a8
Adrian Kunz <a.kunz@uni-kassel.de>
2026-05-09 17:31:00 +0200

    fix(frontend): KcUserPipe cache

--- apps/frontend/src/app/pipes/kc-user.pipe.ts
+++ apps/frontend/src/app/pipes/kc-user.pipe.ts
@@ -1,13 +1,21 @@
 import {inject, Pipe, PipeTransform} from '@angular/core';
 import {KeycloakProfile} from 'keycloak-js';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
+import {tap} from 'rxjs/operators';
 import {KeycloakService} from '../poll/services/keycloak.service';
 
 @Pipe({name: 'kcUser'})
 export class KcUserPipe implements PipeTransform {
   private readonly keycloakService = inject(KeycloakService);
 
+  private readonly cache = new Map<string, KeycloakProfile>();
+
   transform(value: string): Observable<KeycloakProfile> {
-    return this.keycloakService.getUser(value);
+    if (this.cache.has(value)) {
+      return of(this.cache.get(value)!);
+    }
+    return this.keycloakService.getUser(value).pipe(
+      tap(user => this.cache.set(value, user)),
+    );
   }
 }
