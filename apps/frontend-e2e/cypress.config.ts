61c75cf94388376e93e802fab96e4bac0f16d5dc
Julian Holfeld <morphclue@gmail.com>
2026-07-19 08:13:07 +0200

    feat: nx g infer-targets: cypress, eslint and jest

--- apps/frontend-e2e/cypress.config.ts
+++ apps/frontend-e2e/cypress.config.ts
@@ -3,7 +3,14 @@ import {defineConfig} from 'cypress';
 
 export default defineConfig({
   e2e: {
-    ...nxE2EPreset(__dirname),
+    ...nxE2EPreset(__dirname, {
+      ciWebServerCommand: 'npx nx run frontend:serve-static',
+      webServerCommands: {
+        default: 'npx nx run frontend:serve:development',
+        production: 'npx nx run frontend:serve:production',
+        ci: 'npx nx run frontend:serve-static',
+      },
+    }),
     baseUrl: 'http://localhost:4200',
     // Please ensure you use `cy.origin()` when navigating between domains and remove this option.
     // See https://docs.cypress.io/app/references/migration-guide#Changes-to-cyorigin
